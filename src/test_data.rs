13c0d566b28b077bd0c83fc2dd68c825b21f5050
franc90 <azarnowski1@gmail.com>
2020-10-20 19:01:44 +0200

    extract test data to separate file

---
+++ src/test_data.rs
@@ -0,0 +1,56 @@
+pub const SET_1: &str = r#"[
+  {
+    "word": "alpaca",
+    "translated": "alpaka",
+    "explanation": "a domesticated mammal especially of Per",
+    "pronunciation": "/al-ˈpa-kə/",
+    "sentence": "The boutique features items made from alpaca fiber..",
+    "type": "noun",
+    "sentence_gap": "The boutique features items made from ______ fiber.",
+    "gap_term": "alpaca"
+  },
+  {
+    "word": "llama",
+    "translated": "lama",
+    "explanation": "wild or domesticated, long-necked, South American mammals related to the camels but smaller and without a hump",
+    "pronunciation": "/lla·ma/",
+    "sentence": "The llama represents the Inca’s sturdy beast of burden.",
+    "type": "noun",
+    "sentence_gap": "The ______s represents the Inca’s sturdy beast of burden.",
+    "gap_term": "llama"
+  },
+  {
+    "word": "puma",
+    "translated": "puma",
+    "explanation": "",
+    "pronunciation": "/pu·ma/",
+    "sentence": "Adult pumas can sometimes weigh over 200 pounds.",
+    "type": "noun",
+    "sentence_gap": "Adult ______s can sometimes weigh over 200 pounds..",
+    "gap_term": "puma"
+  }
+]"#;
+
+
+pub const SET_2: &str = r#"[
+  {
+    "word": "juxtaposition",
+    "translated": "zestawienie (jako kontrast)",
+    "explanation": "the act or an instance of placing two or more things side by side often to compare or contrast or to create an interesting effect",
+    "pronunciation": "/ˌjək-stə-pə-ˈzi-shən/",
+    "sentence": "Then juxtaposition of life and death is obviously important in the play.",
+    "type": "noun",
+    "sentence_gap": "Then ______ of life and death is obviously important in the play.",
+    "gap_term": "juxtaposition"
+  },
+  {
+    "word": "floccinaucinihilipilification",
+    "translated": "jednorazowy akt lub zwyczajowa skłonność do uznawania czegoś za bezwartościowe",
+    "explanation": "To describe, estimate or regard something as worthless.",
+    "pronunciation": "/ˌflɒksɪˌnɒsɪˌnɪhɪlɪˌpɪlɪfɪˈkeɪʃən/",
+    "sentence": "Let me indulge in the floccinaucinihilipilification of EU judges.",
+    "type": "noun",
+    "sentence_gap": "Let me indulge in the ______ of EU judges.",
+    "gap_term": "floccinaucinihilipilification"
+  }
+]"#;
