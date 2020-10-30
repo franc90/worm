5076bdf9bd20211298dc5533840e9b3e43f5fadd
franc90 <azarnowski1@gmail.com>
2020-10-30 18:43:49 +0100

    update HINTS_TEXT

--- src/card/card_ui.rs
+++ src/card/card_ui.rs
@@ -9,7 +9,7 @@ use cursive::views::{Layer, LinearLayout, ResizedView, TextView};
 use crate::card::card_data::CardSet;
 
 const CARD_LAYOUT_NAME: &str = "card_layout";
-const HINTS_TEXT: &str = "| q:quit | ?:help ";
+const HINTS_TEXT: &str = "| q:quit | ?:help |";
 const DESCRIPTION_PREFIX: &str = "Description: ";
 const EXAMPLE_PREFIX: &str = "Example: ";
 
